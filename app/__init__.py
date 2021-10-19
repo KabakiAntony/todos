b5f0dbb14bae7c027e05b134df913715e9b9b0ca
KabakiAntony <kabaki.antony@gmail.com>
2021-10-19 08:53:26 +0300

    added flask migrate and initiated sqlalchemy and marshmallow

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ factory
 """
 from config import DevelopmentConfig
 from flask import Flask
+from flask_migrate import Migrate
+# blueprint imports here
+
+migrate = Migrate(compare_type=True)
 
 
 def create_app():
@@ -11,7 +15,15 @@ def create_app():
     this is my app factory
     """
     app = Flask(__name__)
+    # cors configuration here
     app.config.from_object(DevelopmentConfig())
+
+    from app.api.models import db, ma
+
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
+    # register blueprints here
     app.app_context().push()
 
     return app
