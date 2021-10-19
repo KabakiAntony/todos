8ded0ffd218d88b33c0fd785a04c0a2957d464bc
KabakiAntony <kabaki.antony@gmail.com>
2021-10-19 09:21:07 +0300

    created models package and created todos and users models

---
+++ app/api/models/todos.py
@@ -0,0 +1,29 @@
+from app.api.models import db, ma
+from datetime import datetime
+
+
+class TodosModel(db.Model):
+    """
+    creating the todos model
+    """
+    __tablename__ = "Todos"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String(20), db.ForeignKey("Users.id"))
+    creation_date = db.Column(db.DateTime(), default=datetime.utcnow)
+    todo = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, id, user_id, creation_date, todo):
+        self.id = id
+        self.user_id = user_id
+        self.creation_date = creation_date
+        self.todo = todo
+
+
+class TodosSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "user_id", "creation_date", "todo")
+
+
+todoSchema = TodosSchema()
+todosSchema = TodosSchema(many=True)
---
+++ app/api/models/users.py
@@ -0,0 +1,41 @@
+from app.api.models import db, ma
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Users(db.Model):
+    """
+    creating users model
+    """
+    __tablename__ = "Users"
+    id = db.Column(db.String(20), primary_key=True)
+    email = db.Column(db.String(255), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+    def hash_password(self, password):
+        """
+        given a password string of any form or size
+        hash it in preparation for storage
+        """
+        password_hash = generate_password_hash(str(password))
+        return password_hash
+
+    def compare_password(hashed_password, password):
+        """
+        given a plain string password generate a hash
+        that will be used to compare with the stored
+        password in the database
+        """
+        return check_password_hash(hashed_password, str(password))
+
+
+class UsersSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "email", "password")
+
+
+user_schema = UsersSchema()
