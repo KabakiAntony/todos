f671fc1f7134f073a3f348f1f06e258dc9ff80d4
KabakiAntony <kabaki.antony@gmail.com>
2021-10-19 09:44:30 +0300

    created teardown method for tests

--- tests/todosBaseTest.py
+++ tests/todosBaseTest.py
@@ -1,6 +1,6 @@
 import unittest
 from app import create_app
-from app.api.model import db
+from app.api.models import db
 
 
 class TodosBaseTest(unittest.TestCase):
@@ -11,3 +11,7 @@ class TodosBaseTest(unittest.TestCase):
         self.app.config.from_object('config.TestingConfig')
         self.client = self.app.test_client()
         db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
