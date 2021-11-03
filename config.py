a484c2e912fc5e979a66f267ab4a97ef91c47279
KabakiAntony <kabaki.antony@gmail.com>
2021-11-03 20:04:51 +0300

    Fix Postgres database URLs to be compatible with SQLAlchemy

--- config.py
+++ config.py
@@ -7,20 +7,17 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+        'postgres://', 'postgresql://')
 
 
 class ProductionConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
-        'postgres://', 'postgresql://')
-
+    
 
 class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
-        'postgres://', 'postgresql://')
-
 
 class TestingConfig(Config):
     DEBUG = True
